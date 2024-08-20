090468d6f93ce61cf8a9350e22063eed5707d869
LVKIN <backuplucas6150@gmail.com>
2024-08-20 01:41:35 -0300

    checkpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,15 @@ export default {
 				header: "0px 5px 20px 0px #1C1C1E14",
 			},
 			colors: {
-				"blue-100": "#191971"
+				"blue-100": "#191971",
+				"blue-200": "#2354CC",
+				"pink-100": "#C92CC4",
+				"green-100": "#21BCAE",
+				// border: 2px solid;
+
+// border-image-source: linear-gradient(90deg,  8.49%,  48.91%,  100%);
+
+
 			},
 			keyframes: {
 				sliding: {
